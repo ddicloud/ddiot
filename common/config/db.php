f05bff3d024a66469723b86527c3bef9c7b404e0
chunchun <2192138785@qq.com>
2022-02-28 10:21:51 +0800

    数据库脚本升级

--- common/config/db.php
+++ common/config/db.php
@@ -4,12 +4,12 @@
  * @Author: Wang chunsheng  email:2192138785@qq.com
  * @Date:   2021-01-18 16:51:31
  * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2022-02-28 10:10:42
+ * @Last Modified time: 2022-02-28 10:21:41
  */
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=20220227',
+    'dsn' => 'mysql:host=localhost;dbname=diandi',
     'tablePrefix' => 'dd_',
     'username' => 'root',
     'password' => 'root',
