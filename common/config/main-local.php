5ce299599f8cfc3dfdfec0c0bffad453f51cbb83
wangchunsheng <34767187+yii-diandi@users.noreply.github.com>
2022-03-29 09:34:58 +0800

    增加sqlserver和mongodb的全局配置

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -3,29 +3,17 @@
  * @Author: Wang chunsheng  email:2192138785@qq.com
  * @Date:   2020-05-09 22:51:22
  * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2022-03-25 20:00:36
+ * @Last Modified time: 2022-03-29 09:33:46
  */
 $db = require __DIR__.'/db.php';
+$sqlServer = require __DIR__.'/sqlServer.php';
+$mongodb = require __DIR__.'/mongodb.php';
 
 return [
     'components' => [
         'db' => $db,
-        'db2' => [
-            'class' => 'yii\db\Connection',
-            'driverName' => 'sqlsrv',
-            'dsn' => 'sqlsrv:Server=125.72.69.150,1433;Database=测试库',
-            'username' => 'sa',
-            'password' => 'pqhuatong@BH',
-            'charset' => 'utf8',
-        ],
-        // 'mongodb' => [
-        //     'class' => '\yii\mongodb\Connection',
-        //     'dsn' => 'mongodb://@localhost:27017/mydatabase',
-        //     'options' => [
-        //         'username' => 'ceshi',
-        //         'password' => 'Password',
-        //     ],
-        // ],
+        'sqlServer' => $sqlServer,
+        'mongodb' => $mongodb,
          /* ------ 缓存 ------ **/
         'cache' => [
             'class' => 'yii\redis\Cache',
---
+++ common/config/mongodb.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * @Author: Wang chunsheng  email:2192138785@qq.com
+ * @Date:   2022-03-29 09:32:37
+ * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
+ * @Last Modified time: 2022-03-29 09:33:58
+ */
+return [
+    // 'class' => '\yii\mongodb\Connection',
+    // 'dsn' => 'mongodb://@localhost:27017/mydatabase',
+    // 'options' => [
+    //     'username' => 'ceshi',
+    //     'password' => 'Password',
+    // ],
+];
