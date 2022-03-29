5086e59d201547c2f48b5a3d0b10eb8d539fc978
wangchunsheng <34767187+yii-diandi@users.noreply.github.com>
2022-03-29 09:39:09 +0800

    配置文件修改

--- common/config/sqlServer.php
+++ common/config/sqlServer.php
@@ -4,14 +4,14 @@
  * @Author: Wang chunsheng  email:2192138785@qq.com
  * @Date:   2021-01-18 16:51:31
  * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2022-03-29 09:34:34
+ * @Last Modified time: 2022-03-29 09:38:58
  */
 
 return [
     'class' => 'yii\db\Connection',
     'driverName' => 'sqlsrv',
     'dsn' => 'sqlsrv:Server=*.*.*.*,1433;Database=测试库',
-    'username' => 'sa',
+    'username' => 'root',
     'password' => 'root',
     'charset' => 'utf8',
 ];
