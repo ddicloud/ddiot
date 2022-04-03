206cb76ffa028059ba04a0f99c11db5166027697
chunchun <2192138785@qq.com>
2022-04-03 16:04:36 +0800

    配置文件优化

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -3,11 +3,16 @@
  * @Author: Wang chunsheng  email:2192138785@qq.com
  * @Date:   2020-05-09 22:51:22
  * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2022-03-30 22:15:34
+ * @Last Modified time: 2022-04-03 16:03:16
  */
 $db = require __DIR__.'/db.php';
-$sqlServer = require __DIR__.'/sqlServer.php';
-$mongodb = require __DIR__.'/mongodb.php';
+if (file_exists(__DIR__.'/sqlServer.php')) {
+    $sqlServer = require __DIR__.'/sqlServer.php';
+}
+if (file_exists(__DIR__.'/mongodb.php')) {
+    $mongodb = require __DIR__.'/mongodb.php';
+}
+
 $redis = require __DIR__.'/redis.php';
 
 return [
