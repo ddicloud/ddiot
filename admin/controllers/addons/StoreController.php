6ddc260ca54fc60c587cbf18a61a6c6f80b21296
wangchunsheng <34767187+yii-diandi@users.noreply.github.com>
2022-04-13 11:41:43 +0800

    商户更新bug处理

--- admin/controllers/addons/StoreController.php
+++ admin/controllers/addons/StoreController.php
@@ -4,7 +4,7 @@
  * @Author: Wang chunsheng  email:2192138785@qq.com
  * @Date:   2020-05-11 15:07:52
  * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2022-01-23 19:24:17
+ * @Last Modified time: 2022-04-13 11:41:05
  */
 
 namespace admin\controllers\addons;
@@ -12,33 +12,28 @@ namespace admin\controllers\addons;
 use admin\controllers\AController;
 use admin\models\addons\models\Bloc;
 use admin\models\enums\StoreStatus;
-use Yii;
-use diandi\addons\models\BlocStore;
-use diandi\addons\models\searchs\BlocStoreSearch;
-use yii\web\Controller;
-use yii\web\NotFoundHttpException;
-use yii\filters\VerbFilter;
-use backend\controllers\BaseController;
 use common\helpers\ArrayHelper;
-use common\helpers\CacheHelper;
 use common\helpers\ErrorsHelper;
 use common\helpers\ImageHelper;
 use common\helpers\LevelTplHelper;
 use common\helpers\ResultHelper;
 use common\models\DdRegion;
-use common\models\UserBloc;
+use diandi\addons\models\BlocStore;
+use diandi\addons\models\searchs\BlocStoreSearch;
 use diandi\addons\models\searchs\StoreCategory;
 use diandi\addons\models\StoreLabel;
 use diandi\addons\models\StoreLabelLink;
-use diandi\admin\models\AuthAssignmentGroup;
+use Yii;
+use yii\filters\VerbFilter;
 use yii\web\HttpException;
+use yii\web\NotFoundHttpException;
 
 /**
  * StoreController implements the CRUD actions for BlocStore model.
  */
 class StoreController extends AController
 {
-    public $modelSearchName = "BlocStore";
+    public $modelSearchName = 'BlocStore';
 
     public $modelClass = '';
 
@@ -108,17 +103,13 @@ class StoreController extends AController
         }
     }
 
-
-
     public function actionCategory()
     {
-        $cates = StoreCategory::find()->select(["parent_id", 'category_id', 'name as label', 'category_id as value'])->asArray()->all();
-        $list  = ArrayHelper::itemsMerge($cates, 0,  'category_id', 'parent_id', 'children');
-
+        $cates = StoreCategory::find()->select(['parent_id', 'category_id', 'name as label', 'category_id as value'])->asArray()->all();
+        $list = ArrayHelper::itemsMerge($cates, 0, 'category_id', 'parent_id', 'children');
 
         return ResultHelper::json(200, '获取成功', $list);
     }
-
 
     /**
      * Displays a single BlocStore model.
@@ -134,11 +125,11 @@ class StoreController extends AController
         $BlocStore = new BlocStore([
             'extras' => $this->extras,
         ]);
-        $detail =  $BlocStore::find()->where(['store_id'=>$id])->asArray()->one();
+        $detail = $BlocStore::find()->where(['store_id' => $id])->asArray()->one();
         $detail['logo'] = ImageHelper::tomedia($detail['logo']);
         $detail['extra'] = unserialize($detail['extra']);
         $storage = Yii::$app->params['conf']['oss']['remote_type'];
-        $url ='';
+        $url = '';
         switch ($storage) {
             case 'locai':
                 $url = Yii::$app->request->hostInfo;
@@ -156,9 +147,10 @@ class StoreController extends AController
                 $url = Yii::$app->request->hostInfo;
             break;
         }
-        $detail['config']   = [
-            'attachmentUrl' => $url."/attachment"
+        $detail['config'] = [
+            'attachmentUrl' => $url.'/attachment',
         ];
+
         return ResultHelper::json(200, '获取成功', $detail);
     }
 
@@ -186,30 +178,28 @@ class StoreController extends AController
             'id' => 'category_id',
         ]);
 
-        $link    = new StoreLabelLink();
+        $link = new StoreLabelLink();
 
         if (Yii::$app->request->isPost) {
-
             $data = Yii::$app->request->post();
 
             $data['lng_lat'] = json_encode([
-                "lng" => $data['longitude'],
-                "lat" => $data['latitude']
+                'lng' => $data['longitude'],
+                'lat' => $data['latitude'],
             ]);
 
             if ($model->load($data, '') && $model->save()) {
-
                 $StoreLabelLink = $_GPC['StoreLabelLink'];
 
                 if (!empty($StoreLabelLink['label_id'])) {
                     foreach ($StoreLabelLink['label_id'] as $key => $label_id) {
-                        $_link    = clone  $link;
-                        $bloc_id   = $model->bloc_id;
-                        $store_id  = $model->store_id;
+                        $_link = clone  $link;
+                        $bloc_id = $model->bloc_id;
+                        $store_id = $model->store_id;
                         $data = [
-                            'bloc_id'  => $bloc_id,
+                            'bloc_id' => $bloc_id,
                             'store_id' => $store_id,
-                            'label_id'  => $label_id,
+                            'label_id' => $label_id,
                         ];
                         $_link->setAttributes($data);
                         $_link->save();
@@ -256,75 +246,45 @@ class StoreController extends AController
 
         $model = $this->findModel($id);
         $model['extra'] = unserialize($model['extra']);
-        $link    = new StoreLabelLink();
+        $link = new StoreLabelLink();
 
-        $bloc_id   = $model->bloc_id;
-        $store_id  = $model->store_id;
-
-        if (Yii::$app->request->isPost) {
-            if ($model->load(Yii::$app->request->post()) && $model->save()) {
-
-
-                $StoreLabelLink = $_GPC['StoreLabelLink'];
-                $link->deleteAll([
-                    'store_id' => $store_id
-                ]);
-
-                if (!empty($StoreLabelLink['label_id'])) {
-
-                    foreach ($StoreLabelLink['label_id'] as $key => $label_id) {
-                        $_link    = clone  $link;
-                        $data = [
-                            'bloc_id'  => $bloc_id,
-                            'store_id' => $store_id,
-                            'label_id'  => $label_id,
-                        ];
-                        $_link->setAttributes($data);
-                        $_link->save();
-                    }
-                }
-
-                return $this->redirect(['view', 'id' => $model->store_id, 'bloc_id' => $model->bloc_id]);
-            } else {
-                $error = ErrorsHelper::getModelError($model);
-                Yii::$app->session->setFlash('error', $error);
-
-                return  $this->refresh();
-            }
-        }
-
-        $modelcate = new StoreCategory();
-
-        $Helper = new LevelTplHelper([
-            'pid' => 'parent_id',
-            'cid' => 'category_id',
-            'title' => 'name',
-            'model' => $modelcate,
-            'id' => 'category_id',
-        ]);
-
-        $labels = StoreLabel::find()->select(['id', 'name'])->indexBy('id')->asArray()->all();
+        $bloc_id = $model->bloc_id;
         $store_id = $model->store_id;
 
-        $linkValue = $link->find()->where(['store_id' => $store_id])->select('label_id')->column();
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            $StoreLabelLink = $_GPC['StoreLabelLink'];
+            $link->deleteAll([
+                    'store_id' => $store_id,
+                ]);
 
-        return ResultHelper::json(200, '获取成功', [
-            'link' => $link,
-            'linkValue' => $linkValue,
-            'labels' => $labels,
-            'bloc_id' => $this->bloc_id,
-            'Helper' => $Helper,
-            'model' => $model,
-        ]);
+            if (!empty($StoreLabelLink['label_id'])) {
+                foreach ($StoreLabelLink['label_id'] as $key => $label_id) {
+                    $_link = clone  $link;
+                    $data = [
+                            'bloc_id' => $bloc_id,
+                            'store_id' => $store_id,
+                            'label_id' => $label_id,
+                        ];
+                    $_link->setAttributes($data);
+                    $_link->save();
+                }
+            }
+
+            return ResultHelper::json(200, '更新成功');
+        } else {
+            $error = ErrorsHelper::getModelError($model);
+
+            return ResultHelper::json(401, $error);
+        }
     }
 
     public function actionBlocs()
     {
         $model = new Bloc();
 
-        $lists = $model->find()->select(["bloc_id", 'pid', 'business_name as label', 'bloc_id as id'])->asArray()->all();
+        $lists = $model->find()->select(['bloc_id', 'pid', 'business_name as label', 'bloc_id as id'])->asArray()->all();
 
-        $list =  ArrayHelper::itemsMerge($lists, 0, "bloc_id", 'pid', 'children');
+        $list = ArrayHelper::itemsMerge($lists, 0, 'bloc_id', 'pid', 'children');
 
         return ResultHelper::json(200, '获取成功', $list);
     }
@@ -337,8 +297,8 @@ class StoreController extends AController
 
         foreach ($list as $key => $value) {
             $lists[] = [
-                "text" => $value,
-                "value" => $key
+                'text' => $value,
+                'value' => $key,
             ];
         }
 
