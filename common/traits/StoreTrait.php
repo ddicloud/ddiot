15a27a5bdbf16cbe9ddb37edfd696b4c0216e13b
wangchunsheng <34767187+yii-diandi@users.noreply.github.com>
2022-04-22 14:49:32 +0800

    数据分离查询

--- common/traits/StoreTrait.php
+++ common/traits/StoreTrait.php
@@ -3,13 +3,17 @@
  * @Author: Wang chunsheng  email:2192138785@qq.com
  * @Date:   2022-04-22 14:40:19
  * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2022-04-22 14:42:04
+ * @Last Modified time: 2022-04-22 14:49:21
  */
 
 namespace common\traits;
 
+use yii\db\QueryTrait;
+
 trait StoreTrait
 {
+    use QueryTrait;
+
     public function findBloc()
     {
     }
