8947af5b2d39a1d3d081e6e7122234c21939f54b
wangchunsheng <34767187+yii-diandi@users.noreply.github.com>
2022-04-22 14:43:42 +0800

    数据分离全局方式处理

---
+++ common/traits/StoreTrait.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @Author: Wang chunsheng  email:2192138785@qq.com
+ * @Date:   2022-04-22 14:40:19
+ * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
+ * @Last Modified time: 2022-04-22 14:42:04
+ */
+
+namespace common\traits;
+
+trait StoreTrait
+{
+    public function findBloc()
+    {
+    }
+
+    public function findStore()
+    {
+    }
+}
