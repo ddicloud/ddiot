e858478d0491744503c095456f648593643495ca
wangchunsheng <34767187+yii-diandi@users.noreply.github.com>
2022-04-27 15:17:20 +0800

    公众号消息处理

--- api/modules/officialaccount/controllers/MsgController.php
+++ api/modules/officialaccount/controllers/MsgController.php
@@ -3,23 +3,19 @@
  * @Author: Wang chunsheng  email:2192138785@qq.com
  * @Date:   2020-11-14 22:17:14
  * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2020-11-20 00:25:03
+ * @Last Modified time: 2022-04-27 15:17:10
  */
- 
 
 namespace api\modules\officialaccount\controllers;
 
 use api\controllers\AController;
-use Yii;
 use app\modules\officialaccount\components\Fans;
 use common\helpers\FileHelper;
+use Yii;
 use yii\web\NotFoundHttpException;
-
-
 
 class MsgController extends AController
 {
-    
     protected $authOptional = ['index'];
 
     public $modelClass = 'api\modules\officialaccount\models\DdWechatFans';
@@ -32,31 +28,27 @@ class MsgController extends AController
     public $enableCsrfValidation = false;
 
     /**
-     * 只做微信公众号激活，不做其他消息处理
-     *
+     * 只做微信公众号激活，不做其他消息处理.
      */
     public function actionIndex()
     {
         global $_GPC;
         $request = Yii::$app->request;
         $logPath = Yii::getAlias('@runtime/wechat/msg/'.date('ymd').'.log');
-        
+
         FileHelper::writeLog($logPath, '公众号激活验证');
         $app = Yii::$app->wechat->getApp();
-        if($request->getMethod() ==  'GET'){
-            FileHelper::writeLog($logPath, '开始验证');
-            FileHelper::writeLog($logPath, '验证参数：'.json_encode([
-                $request->get()
-            ]));
+        FileHelper::writeLog($logPath, '开始验证');
+        FileHelper::writeLog($logPath, '验证参数：'.json_encode([
+            $request->get(),
+        ]));
 
-            if (Fans::verifyToken($request->get('signature'), $request->get('timestamp'), $request->get('nonce'))) {
-                FileHelper::writeLog($logPath, '验证通过');
-                $response = $app->server->serve();
-                $response->send();
-            }
-            throw new NotFoundHttpException('签名验证失败.');    
+        if (Fans::verifyToken($request->get('signature'), $request->get('timestamp'), $request->get('nonce'))) {
+            FileHelper::writeLog($logPath, '验证通过');
+            $response = $app->server->serve();
+            $response->send();
         }
-
+        throw new NotFoundHttpException('签名验证失败.');
         exit();
     }
 }
