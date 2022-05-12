ee307bc5a2c4a8ce0f5898649a463fd39d1a0966
wangchunsheng <34767187+yii-diandi@users.noreply.github.com>
2022-05-12 19:51:44 +0800

    行为日志处理

--- common/behaviors/ServiceBehavior.php
+++ common/behaviors/ServiceBehavior.php
@@ -4,7 +4,7 @@
  * @Author: Wang chunsheng  email:2192138785@qq.com
  * @Date:   2020-05-15 22:50:42
  * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2022-05-12 19:43:21
+ * @Last Modified time: 2022-05-12 19:51:35
  */
 
 namespace common\behaviors;
@@ -48,7 +48,12 @@ class ServiceBehavior extends Behavior
             'class' => $namespace.$serviceClassName,
         ]);
         $action = $event->action;
+        try {
+            $Res = call_user_func_array([$service, $action], $event->params);
 
-        return  call_user_func_array([$service, $action], $event->params);
+            return $Res;
+        } catch (\Exception $e) {
+            throw $e;
+        }
     }
 }
