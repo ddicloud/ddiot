3ae1be2f1b8b4160f86559d7673adb3ad7cf3592
wangchunsheng <34767187+yii-diandi@users.noreply.github.com>
2022-05-12 17:15:24 +0800

    业务扩展接口处理

--- admin/config/main.php
+++ admin/config/main.php
@@ -4,7 +4,7 @@
  * @Author: Wang chunsheng  email:2192138785@qq.com
  * @Date:   2021-04-27 03:17:29
  * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2022-05-12 16:43:34
+ * @Last Modified time: 2022-05-12 17:14:49
  */
 $params = array_merge(
     require __DIR__.'/../../common/config/params.php',
@@ -291,6 +291,7 @@ return [
                         'POST,GET info' => 'info',
                         'POST create' => 'create',
                         'POST update' => 'update',
+                        'POST uninstalled' => 'uninstalled',
                     ],
                 ],
                 [
