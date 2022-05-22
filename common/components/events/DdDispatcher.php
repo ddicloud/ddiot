f8997ae6e6200fc94992108f13c7654792efb1fe
chunchun <2192138785@qq.com>
2022-05-22 15:32:36 +0800

    1

--- common/components/events/DdDispatcher.php
+++ common/components/events/DdDispatcher.php
@@ -3,10 +3,11 @@
  * @Author: Wang chunsheng  email:2192138785@qq.com
  * @Date:   2022-05-22 15:21:47
  * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2022-05-22 15:23:20
+ * @Last Modified time: 2022-05-22 15:32:28
  */
 namespace common\components\events;
 use Symfony\Component\EventDispatcher\EventDispatcher;
+
 
 class DdDispatcher extends EventDispatcher
 {
