ac9e11a5716f4442dacf05426926e17002d20113
wangchunsheng <34767187+yii-diandi@users.noreply.github.com>
2022-05-23 12:33:05 +0800

    1

--- addons/diandi_example/services/ParentEventServer.php
+++ addons/diandi_example/services/ParentEventServer.php
@@ -4,7 +4,7 @@
  * @Author: Wang chunsheng  email:2192138785@qq.com
  * @Date:   2021-01-22 22:11:27
  * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2022-05-23 12:25:20
+ * @Last Modified time: 2022-05-23 12:32:53
  */
 
 namespace addons\diandi_example\services;
@@ -17,11 +17,10 @@ class ParentEventServer extends BaseService
 {
     const EVENT_LOCK_OPEN = 'foo.method_is_not_found';
 
-    public function ceshi($event)
+    public function ceshi($params)
     {
         echo '父级第一个输出';
        
-        print_r($event);
-        // return ['data' => $params];
+        return ['data' => $params];
     }
 }
