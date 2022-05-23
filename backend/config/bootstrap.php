09e6a512ea92415c8c3685cdbcbefc29e74d4168
wangchunsheng <34767187+yii-diandi@users.noreply.github.com>
2022-05-23 19:17:04 +0800

    去掉backend后台

--- backend/config/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * @Author: Wang chunsheng  email:2192138785@qq.com
- * @Date:   2021-04-27 03:18:03
- * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2021-05-17 19:32:05
- */
- 
--- backend/views/site/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-/* @var $this yii\web\View */
-
-$this->title = '店滴新零售管理系统';
