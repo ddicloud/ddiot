63611b0d12136a5597b5fa16d3f7ebc9e81cb93c
wangchunsheng <34767187+yii-diandi@users.noreply.github.com>
2022-05-24 11:20:57 +0800

    12

--- addons/diandi_example/config/api.php
+++ addons/diandi_example/config/api.php
@@ -4,13 +4,13 @@
  * @Author: Wang chunsheng  email:2192138785@qq.com
  * @Date:   2021-06-08 17:33:49
  * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2022-05-24 11:20:43
+ * @Last Modified time: 2022-05-24 11:20:51
  */
 
 return [
     [
         'class' => 'yii\rest\UrlRule',
-        'controller' => ['diandi_example/task'], //模块名称/控制器方法
+        'controller' => ['diandi_example/api'], //模块名称/控制器方法
         'pluralize' => false,
         'extraPatterns' => [
             'POST uptable' => 'uptable', //请求方式 请求方法，映射方法
