3cd88e294b12f8720cc9abdab74812938048c4e0
wangchunsheng <34767187+yii-diandi@users.noreply.github.com>
2022-05-24 14:00:11 +0800

    1

--- addons/diandi_example/config/console.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'controllerMap' => [
-        'cron' => [
-            'class' => 'addons\diandi_task\console\swoole\CronController',//生成 php ./yii cron/action
-        ]
-    ]
-]; 
--- addons/diandi_example/views/default/index.php
+++
@@ -1,9 +0,0 @@
-<div class="diandi_example-default-index">
-
-  
-<div class="jumbotron text-muted">
-    <h1 class="def-title"><?=  $info['title']; ?></h1>
-    <p class="def-wel">Welcome To <?=  $info['identifie']; ?></p>
-    <p class="del-bottom">欢迎您，使用<?=  $info['description']; ?></p>
-  </div>
-</div>
