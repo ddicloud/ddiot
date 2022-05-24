40cea8b849b6ab0872e650ae2391113362870851
wangchunsheng <34767187+yii-diandi@users.noreply.github.com>
2022-05-24 14:44:42 +0800

    事件增加批量派遣

--- common/components/events/DdDispatcher.php
+++ common/components/events/DdDispatcher.php
@@ -3,7 +3,7 @@
  * @Author: Wang chunsheng  email:2192138785@qq.com
  * @Date:   2022-05-22 15:21:47
  * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2022-05-22 16:03:15
+ * @Last Modified time: 2022-05-24 14:44:26
  */
 namespace common\components\events;
 use Symfony\Component\EventDispatcher\EventDispatcher;
@@ -17,7 +17,13 @@ use Symfony\Component\EventDispatcher\EventDispatcher;
  */
 class DdDispatcher extends EventDispatcher
 {
-    
+    // 批量派遣
+    public function dispatchs(array $events)
+    {
+        foreach ($events as $key => $value) {
+            $this->dispatch($value);
+        }
+    }
 }
 
 
