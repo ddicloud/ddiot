62b1a1a27add088d224fea4aa3fd00de9b7635e8
wangchunsheng <34767187+yii-diandi@users.noreply.github.com>
2022-05-24 14:46:26 +0800

    批量注册事件

--- common/components/events/DdDispatcher.php
+++ common/components/events/DdDispatcher.php
@@ -3,7 +3,7 @@
  * @Author: Wang chunsheng  email:2192138785@qq.com
  * @Date:   2022-05-22 15:21:47
  * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2022-05-24 14:44:26
+ * @Last Modified time: 2022-05-24 14:45:50
  */
 namespace common\components\events;
 use Symfony\Component\EventDispatcher\EventDispatcher;
@@ -18,10 +18,10 @@ use Symfony\Component\EventDispatcher\EventDispatcher;
 class DdDispatcher extends EventDispatcher
 {
     // 批量派遣
-    public function dispatchs(array $events)
+    public function dispatchs(array $events,$event)
     {
         foreach ($events as $key => $value) {
-            $this->dispatch($value);
+            $this->dispatch($value,$event);
         }
     }
 }
