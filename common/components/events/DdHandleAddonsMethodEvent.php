8526baf87bab7ee9d777fb549d3292668cf52096
wangchunsheng <34767187+yii-diandi@users.noreply.github.com>
2022-05-25 18:41:48 +0800

    事件服务层调度器处理

--- common/components/events/DdHandleAddonsMethodEvent.php
+++ common/components/events/DdHandleAddonsMethodEvent.php
@@ -3,19 +3,22 @@
  * @Author: Wang chunsheng  email:2192138785@qq.com
  * @Date:   2022-05-23 09:33:48
  * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2022-05-23 12:28:51
+ * @Last Modified time: 2022-05-25 18:41:36
  */
+
 namespace common\components\events;
 
-
-use Symfony\Component\EventDispatcher\Event;
 use common\components\events\eventObjs\DdEvent;
- 
+
 /**
- * 模块事件统一调度
+ * 模块事件统一调度.
+ *
  * @date 2022-05-23
+ *
  * @example
+ *
  * @author Wang Chunsheng
+ *
  * @since
  */
 class DdHandleAddonsMethodEvent extends DdEvent
@@ -25,7 +28,7 @@ class DdHandleAddonsMethodEvent extends DdEvent
     protected $arguments;
     protected $returnValue;
     protected $isProcessed = false;
- 
+
     public function __construct($subject, $method, $arguments)
     {
         $this->subject = $subject;
@@ -33,25 +36,25 @@ class DdHandleAddonsMethodEvent extends DdEvent
         $this->arguments = $arguments;
         echo '调度开始'.PHP_EOL;
     }
- 
+
     public function getSubject()
     {
         return $this->subject;
     }
- 
+
     public function getMethod()
     {
         return $this->method;
     }
- 
+
     public function getArguments()
     {
         return $this->arguments;
     }
- 
+
     /**
      * Sets the value to return and stops other listeners from being notified
-     * 设置返回值，同时中止向其他监听发送通知
+     * 设置返回值，同时中止向其他监听发送通知.
      */
     public function setReturnValue($val)
     {
@@ -59,21 +62,22 @@ class DdHandleAddonsMethodEvent extends DdEvent
         $this->isProcessed = true;
         $this->stopPropagation();
     }
- 
+
     public function getReturnValue()
     {
-        $action  = $this->getMethod();
+        $action = $this->getMethod();
         $service = $this->getSubject();
         // $this->getArguments()
         // call_user_func();
-        $Res = call_user_func_array([$service, $action], [$this->getArguments()]);
+        // $Res = call_user_func_array([$service, $action], [$this->getArguments()]);
+        $Res = $service->$action($this->getArguments());
+        // call_user_func_array([$service, $action], [$this->getArguments()]);
+
         return $Res;
     }
- 
+
     public function isProcessed()
     {
         return $this->isProcessed;
     }
-
-   
 }
