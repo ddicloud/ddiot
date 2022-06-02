d72de29ad849d70ef5de214b2e6f9945ba96490a
wangchunsheng <34767187+yii-diandi@users.noreply.github.com>
2022-06-02 16:56:55 +0800

    控制台request增加魔术变量

--- console/services/request.php
+++ console/services/request.php
@@ -5,7 +5,7 @@
  * @Author: Wang chunsheng  email:2192138785@qq.com
  * @Date:   2022-06-02 16:48:20
  * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2022-06-02 16:55:58
+ * @Last Modified time: 2022-06-02 16:56:40
  */
 namespace console\services;
 
@@ -13,6 +13,11 @@ use yii\console\Request as ConsoleRequest;
 
 class request extends ConsoleRequest
 {
+    public function __call($name, $params)
+    {
+        
+    }
+    
     function getAbsoluteUrl(){
         
     }
