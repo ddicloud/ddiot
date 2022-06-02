3db7b84c479480b3df61a7471b1f258297ce4641
wangchunsheng <34767187+yii-diandi@users.noreply.github.com>
2022-06-02 18:10:14 +0800

    swoole语言包扩展

---
+++ console/swoole/messages/zh-CN/app.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @Author: Wang chunsheng  email:2192138785@qq.com
+ * @Date:   2022-06-02 18:09:36
+ * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
+ * @Last Modified time: 2022-06-02 18:09:50
+ */
+
+return [
+    'Save' => '保存'
+];
