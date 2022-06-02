53fb83829d9ca5131decb6dcd89bd163169c255d
wangchunsheng <34767187+yii-diandi@users.noreply.github.com>
2022-06-02 16:56:04 +0800

    12

--- console/services/request.php
+++ console/services/request.php
@@ -5,7 +5,7 @@
  * @Author: Wang chunsheng  email:2192138785@qq.com
  * @Date:   2022-06-02 16:48:20
  * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2022-06-02 16:52:03
+ * @Last Modified time: 2022-06-02 16:55:58
  */
 namespace console\services;
 
@@ -14,6 +14,11 @@ use yii\console\Request as ConsoleRequest;
 class request extends ConsoleRequest
 {
     function getAbsoluteUrl(){
+        
+    }
+
+    public function getIsAjax()
+    {
         
     }
 }
