4f7d17d2f29d4947a65956ee20534a72afb0399e
wangchunsheng <34767187+yii-diandi@users.noreply.github.com>
2022-06-05 10:47:50 +0800

    12

--- console/services/response.php
+++ console/services/response.php
@@ -3,7 +3,7 @@
  * @Author: Wang chunsheng  email:2192138785@qq.com
  * @Date:   2022-06-05 10:25:54
  * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2022-06-05 10:26:47
+ * @Last Modified time: 2022-06-05 10:47:46
  */
 namespace console\services;
 
@@ -12,4 +12,5 @@ use yii\console\Response as ConsoleResponse;
 class response extends ConsoleResponse
 {
     public $cookies = [];
+    public $statusCode;
 }
