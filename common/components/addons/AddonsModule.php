feb87a36947d558423f63ca29fdbff531de417c8
wangchunsheng <34767187+yii-diandi@users.noreply.github.com>
2022-06-05 11:19:30 +0800

    swoole日志权限处理

--- common/components/addons/AddonsModule.php
+++ common/components/addons/AddonsModule.php
@@ -4,7 +4,7 @@
  * @Author: Wang Chunsheng 2192138785@qq.com
  * @Date:   2020-03-26 09:30:21
  * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2022-06-05 11:17:50
+ * @Last Modified time: 2022-06-05 11:19:21
  */
 
 namespace common\components\addons;
@@ -87,7 +87,6 @@ class AddonsModule extends Module
                 $files = ['baseserver.log','baseserver.pid','swoole.log','swoole.log'];
                 foreach ($files as $key => $value) {
                     if(!file_exists($runtimePath.'/'.$value)){
-                        chmod($runtimePath.'/'.$value, 0777);
                         file_put_contents($runtimePath.'/'.$value,'');
                     }
                 }
