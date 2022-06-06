e7ad9635464bd3c63c5eb5744d4e71344603438f
wangchunsheng <34767187+yii-diandi@users.noreply.github.com>
2022-06-06 11:35:29 +0800

    好的

--- common/helpers/UrlHelper.php
+++ common/helpers/UrlHelper.php
@@ -4,7 +4,7 @@
  * @Author: Wang Chunsheng 2192138785@qq.com
  * @Date:   2020-03-03 09:53:09
  * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2022-06-06 11:29:01
+ * @Last Modified time: 2022-06-06 11:31:23
  */
 
 namespace common\helpers;
@@ -19,8 +19,8 @@ use yii\helpers\Json;
  */
 class UrlHelper extends BaseUrl
 {
-   public static function addonsApiUrl($addons,$controller,$action,$options=[])
+   public static function addonsUrl($addons,$controller,$action,$options=[])
    {
-       return self::toRoute('//api/'.$addons.'/'.$controller.'/'.$action);
+       return self::toRoute([$addons.'/'.$controller.'/'.$action]);
    }
 }
