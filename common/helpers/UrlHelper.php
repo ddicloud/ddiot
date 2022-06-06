386a614effba676c06a0ad69d509de011991bf5c
wangchunsheng <34767187+yii-diandi@users.noreply.github.com>
2022-06-06 11:55:26 +0800

    url助手处理

--- common/helpers/UrlHelper.php
+++ common/helpers/UrlHelper.php
@@ -4,13 +4,12 @@
  * @Author: Wang Chunsheng 2192138785@qq.com
  * @Date:   2020-03-03 09:53:09
  * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2022-06-06 11:52:34
+ * @Last Modified time: 2022-06-06 11:55:19
  */
 
 namespace common\helpers;
 
 use yii\helpers\BaseUrl;
-use yii\helpers\Json;
 
 /**
  * Class ArrayHelper.
@@ -19,12 +18,14 @@ use yii\helpers\Json;
  */
 class UrlHelper extends BaseUrl
 {
-   public static function addonsUrl($addons,$controller,$action,$options=[])
-   {
-       $urlArr =  ['/'.$addons.'/'.$controller.'/'.$action];
-       foreach ($options as $key => $value) {
-            array_push($urlArr,$value);
-       }
-       return self::to($urlArr);
-   }
+    public static function addonsUrl($addons, $controller, $action, $options = [])
+    {
+        global $_GPC;
+        $urlArr = ['/'.$addons.'/'.$controller.'/'.$action];
+        foreach ($options as $key => $value) {
+            $urlArr = array_merge($urlArr, [$key => $value]);
+        }
+
+        return self::to($urlArr);
+    }
 }
