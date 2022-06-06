9e499dcaad66c91f16ff34d8d07de5a5407e23e0
wangchunsheng <34767187+yii-diandi@users.noreply.github.com>
2022-06-06 11:52:43 +0800

    url助手修復

--- common/helpers/UrlHelper.php
+++ common/helpers/UrlHelper.php
@@ -4,7 +4,7 @@
  * @Author: Wang Chunsheng 2192138785@qq.com
  * @Date:   2020-03-03 09:53:09
  * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2022-06-06 11:48:04
+ * @Last Modified time: 2022-06-06 11:52:34
  */
 
 namespace common\helpers;
@@ -22,7 +22,9 @@ class UrlHelper extends BaseUrl
    public static function addonsUrl($addons,$controller,$action,$options=[])
    {
        $urlArr =  ['/'.$addons.'/'.$controller.'/'.$action];
-       array_push($urlArr,$options);
+       foreach ($options as $key => $value) {
+            array_push($urlArr,$value);
+       }
        return self::to($urlArr);
    }
 }
