5664dedab4ea012b8f2f99afc3f85a3a17d657b0
wangchunsheng <34767187+yii-diandi@users.noreply.github.com>
2022-06-06 09:56:19 +0800

    文件权限bug处理

--- common/helpers/FileHelper.php
+++ common/helpers/FileHelper.php
@@ -4,7 +4,7 @@
  * @Author: Wang Chunsheng 2192138785@qq.com
  * @Date:   2020-04-01 05:26:26
  * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2022-06-06 09:47:47
+ * @Last Modified time: 2022-06-06 09:56:08
  */
 
 
@@ -29,7 +29,6 @@ class FileHelper extends BaseFileHelper
     {
         if (!file_exists($catalogue)) {
             self::mkdirs(dirname($catalogue));
-            @chmod(dirname($catalogue), 0777);
             mkdir($catalogue, 0777);
         }
 
