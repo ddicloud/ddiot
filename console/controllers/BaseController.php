8217832399a5ed34dc5a430203796628649b3468
wangchunsheng <34767187+yii-diandi@users.noreply.github.com>
2022-06-06 09:40:24 +0800

    SWOOLE_RUNTIME 全局变量初始化处理

--- console/controllers/BaseController.php
+++ console/controllers/BaseController.php
@@ -3,11 +3,12 @@
  * @Author: Wang chunsheng  email:2192138785@qq.com
  * @Date:   2021-01-25 12:30:32
  * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2021-03-19 18:02:08
+ * @Last Modified time: 2022-06-06 09:40:00
  */
  
 namespace console\controllers;
 
+use common\helpers\FileHelper;
 use Yii;
 
 class BaseController extends \yii\console\Controller
@@ -24,6 +25,20 @@ class BaseController extends \yii\console\Controller
     {
         Yii::$app->service->commonGlobalsService->initId($this->bloc_id, $this->store_id, $this->addons);
         Yii::$app->service->commonGlobalsService->getConf($this->bloc_id);
+        $module = $this->addons;
+        $runtimePath = Yii::getAlias('@app/runtime/'.$module.'/swoole');
+        define('SWOOLE_RUNTIME',$runtimePath);
+        FileHelper::mkdirs($runtimePath);
+        if(is_dir($runtimePath)){
+            @chmod($runtimePath, 0777);
+        }
+        $files = ['baseserver.log','baseserver.pid','swoole.log','swoole.log'];
+        foreach ($files as $key => $value) {
+            if(!file_exists($runtimePath.'/'.$value)){
+                file_put_contents($runtimePath.'/'.$value,'');
+                @chmod($runtimePath.'/'.$value, 0777);
+            }
+        }
     }
     
     public function options($actionID)
