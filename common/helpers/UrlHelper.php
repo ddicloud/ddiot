7d8848466f33ff2948fb41d1e8e3518ae4751c91
wangchunsheng <34767187+yii-diandi@users.noreply.github.com>
2022-06-06 11:29:07 +0800

    地址处理

--- common/helpers/UrlHelper.php
+++ common/helpers/UrlHelper.php
@@ -4,7 +4,7 @@
  * @Author: Wang Chunsheng 2192138785@qq.com
  * @Date:   2020-03-03 09:53:09
  * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2022-06-06 11:17:18
+ * @Last Modified time: 2022-06-06 11:29:01
  */
 
 namespace common\helpers;
@@ -21,6 +21,6 @@ class UrlHelper extends BaseUrl
 {
    public static function addonsApiUrl($addons,$controller,$action,$options=[])
    {
-       return self::toRoute('api/'.$addons.'/'.$controller.'/'.$action);
+       return self::toRoute('//api/'.$addons.'/'.$controller.'/'.$action);
    }
 }
