3498489c4df4be0074884be78bd525404e5fe16b
chunchun <2192138785@qq.com>
2022-06-11 16:53:36 +0800

    权限命令修复

--- console/controllers/ChmodController.php
+++ console/controllers/ChmodController.php
@@ -4,14 +4,12 @@
  * @Author: Wang chunsheng  email:2192138785@qq.com
  * @Date:   2020-07-02 12:49:11
  * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2021-10-14 14:00:45
+ * @Last Modified time: 2022-06-11 16:53:11
  */
 
 namespace console\controllers;
 
-use common\services\backend\NavService;
 use Yii;
-use yii\console\Controller;
 
 // 使用示例： ./yii addons -addons=diandi_lottery -bloc_id=1 -store_id=3   job ninini
 
@@ -19,18 +17,18 @@ class ChmodController extends \yii\console\Controller
 {
     public function actionIndex()
     {
-        $baseDir = dirname(__FILE__) . '/../../';
+        $baseDir = dirname(__FILE__).'/../../';
 
-        $dirs = ["api/runtime/", "backend/assets/", "backend/runtime/", "frontend/runtime/", "frontend/web/assets/", "frontend/web/backend/assets", "frontend/web/attachment", "console/swoole/runtime/", "admin/runtime/"];
+        $dirs = ['api/runtime/', 'frontend/runtime/', 'frontend/web/assets/', 'frontend/web/backend/assets', 'frontend/web/attachment', 'console/swoole/runtime/', 'admin/runtime/'];
 
         foreach ($dirs as $key => $value) {
-            if (is_dir($baseDir . $value)) {
-                chmod($baseDir . $value, 0777);
+            if (is_dir($baseDir.$value)) {
+                chmod($baseDir.$value, 0777);
             } else {
-                mkdir($baseDir . $value, 0777);
-                chmod($baseDir . $value, 0777);
+                mkdir($baseDir.$value, 0777);
+                chmod($baseDir.$value, 0777);
             }
-            echo "目录" . $value . '权限设置成功' . PHP_EOL;
+            echo '目录'.$value.'权限设置成功'.PHP_EOL;
             sleep(1);
         }
     }
