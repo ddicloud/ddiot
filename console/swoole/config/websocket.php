7f48791495013e6938651eddfdd967a6c0d415d2
wangchunsheng <34767187+yii-diandi@users.noreply.github.com>
2022-06-14 16:13:13 +0800

    swoole服务优化

--- console/swoole/config/websocket.php
+++ console/swoole/config/websocket.php
@@ -4,7 +4,7 @@
  * @Author: Wang chunsheng  email:2192138785@qq.com
  * @Date:   2021-01-19 20:27:15
  * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2021-09-06 17:36:29
+ * @Last Modified time: 2022-06-14 16:12:52
  */
 return [
     'host' => '0.0.0.0',
@@ -12,10 +12,10 @@ return [
     'mode' => SWOOLE_PROCESS,
     'sockType' => SWOOLE_SOCK_TCP,
     'type' => 'ws',
-    'app' => require __DIR__ . '/web.php',
+    'app' => require __DIR__.'/web.php',
     'options' => [
-        'pid_file' => __DIR__ . '/../runtime/websocket.pid',
-        'log_file' => __DIR__ . '/../runtime/websocket.log',
+        'pid_file' => __DIR__.'/../runtime/websocket.pid',
+        'log_file' => __DIR__.'/../runtime/websocket.log',
         'debug_mode' => 1,
         'user' => 'www',
         'group' => 'www',
@@ -23,8 +23,5 @@ return [
         'worker_num' => 2,
         'daemonize' => 0,
         'task_worker_num' => 10,
-        'onMessage' => [
-            'class' => 'weewew'
-        ]
-    ]
+    ],
 ];
