44ce2d0ef8fe5f93eb6b8df01db2940cbb6ee555
wangchunsheng <34767187+yii-diandi@users.noreply.github.com>
2022-06-21 19:04:05 +0800

    增加应用授权处理

--- common/config/diandi.php
+++ common/config/diandi.php
@@ -4,7 +4,7 @@
  * @Author: Wang chunsheng  email:2192138785@qq.com
  * @Date:   2021-01-18 16:51:31
  * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2022-06-21 14:40:38
+ * @Last Modified time: 2022-06-21 18:57:43
  */
 
 return [
