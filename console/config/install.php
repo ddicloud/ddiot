6e55026dd9638d0c870a6ea66840cb6e7c30b724
wangchunsheng <34767187+yii-diandi@users.noreply.github.com>
2022-06-27 11:18:43 +0800

    安装优化

--- console/config/install.php
+++ console/config/install.php
@@ -4,7 +4,7 @@
  * @Author: Wang chunsheng  email:2192138785@qq.com
  * @Date:   2021-01-18 16:51:31
  * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2022-06-27 10:47:58
+ * @Last Modified time: 2022-06-27 11:18:35
  */
 
 /**
@@ -20,7 +20,6 @@ return [
     'dbusername' => '',
     'dbpassword' => '',
     // 管理员配置
-    'password' => '',
     'username' => '',
     'userpassword'=>'',    
     'mobile' => '',
