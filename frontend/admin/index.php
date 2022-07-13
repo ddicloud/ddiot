784236b30d6d7d83af43243a4f34e5b31ba61420
wangchunsheng <34767187+yii-diandi@users.noreply.github.com>
2022-07-13 15:29:54 +0800

    自动区分开发环境和测试环境

--- frontend/admin/index.php
+++ frontend/admin/index.php
@@ -4,10 +4,17 @@
  * @Author: Wang chunsheng  email:2192138785@qq.com
  * @Date:   2020-05-15 20:27:36
  * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2022-06-28 16:43:47
+ * @Last Modified time: 2022-07-13 15:29:37
  */
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
+if (in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1'])) {
+    //开发环境dev的入口文件代码
+    defined('YII_DEBUG') or define('YII_DEBUG', true);
+    defined('YII_ENV') or define('YII_ENV', 'dev');
+} else {
+    //生产环境prod的入口文件代码
+    defined('YII_DEBUG') or define('YII_DEBUG', false);
+    defined('YII_ENV') or define('YII_ENV', 'prod');
+}
 
 require __DIR__.'/../../vendor/autoload.php';
 require __DIR__.'/../../vendor/yiisoft/yii2/Yii.php';
