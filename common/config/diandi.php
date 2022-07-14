b4fe60341113fae599398ce69d656450618772a6
wangchunsheng <34767187+yii-diandi@users.noreply.github.com>
2022-07-14 19:11:37 +0800

    授权验证修改

--- common/config/diandi.php
+++ common/config/diandi.php
@@ -4,12 +4,13 @@
  * @Author: Wang chunsheng  email:2192138785@qq.com
  * @Date:   2021-01-18 16:51:31
  * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2022-06-21 18:57:43
+ * @Last Modified time: 2022-07-14 19:05:39
  */
 
 return [
-    'username' => 'chunchun',
-    'password' => '12345678',
+    'username' => '',
+    'password' => '',
+    'webkey' => '',
     'bloc_id' => 1,
     'store_id' => 2,
 ];
