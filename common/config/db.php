5d467afc6a07516571469a60aab7585aae95349b
wangchunsheng <34767187+yii-diandi@users.noreply.github.com>
2022-07-18 11:34:53 +0800

    1

--- common/config/db.php
+++ common/config/db.php
@@ -4,15 +4,15 @@
  * @Author: Wang chunsheng  email:2192138785@qq.com
  * @Date:   2021-01-18 16:51:31
  * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2022-07-15 14:45:30
+ * @Last Modified time: 2022-07-18 11:34:45
  */
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=82.156.131.85;dbname=dev.hopes;port=3306',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=diandicms;port=3306',
     'tablePrefix' => 'dd_',
-    'username' => 'dev.hopes',
-    'password' => 'ceJ2ydXti5FjjMGb',
+    'username' => 'diandicms',
+    'password' => '12345678',
     'charset' => 'utf8',
     'attributes' => [
         PDO::ATTR_STRINGIFY_FETCHES => false,
