40e6622353ceb2aa08fc9094529c6011d4adc467
wangchunsheng <34767187+yii-diandi@users.noreply.github.com>
2022-08-01 16:51:04 +0800

    商戶数据校验权限反馈

--- admin/controllers/addons/BlocController.php
+++ admin/controllers/addons/BlocController.php
@@ -4,7 +4,7 @@
  * @Author: Wang chunsheng  email:2192138785@qq.com
  * @Date:   2021-06-02 17:20:53
  * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2022-06-22 17:49:50
+ * @Last Modified time: 2022-08-01 16:50:48
  */
 
 namespace admin\controllers\addons;
@@ -100,12 +100,26 @@ class BlocController extends AController
 
     public function actionStoreList()
     {
-        $stores = BlocStore::find()->with(['bloc'])->asArray()->all();
+        $user_id = Yii::$app->user->id;
+        $group = AuthAssignmentGroup::find()->where(['user_id' => $user_id])->asArray()->all();
+        $where = [];
+        $list = [];
+        $defaultRoles = Yii::$app->authManager->defaultRoles;
+        // 确定我的权限角色与系统默认有交集，name就显示所有集团
+        $diff = array_intersect($defaultRoles, array_column($group, 'item_name'));
+        $where = [];
+        if (empty($diff)) {
+            // 查找自己的数据
+            $store_ids = array_column($group, 'store_id');
+            $where['store_id'] = $store_ids;
+        }
+
+        $stores = BlocStore::find()->where($where)->with(['bloc'])->asArray()->all();
         foreach ($stores as $key => &$value) {
             $value['logo'] = ImageHelper::tomedia($value['logo']);
         }
 
-        return ResultHelper::json(200, '获取成功',$stores);
+        return ResultHelper::json(200, '获取成功', $stores);
     }
 
     /**
