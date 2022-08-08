2e50aee304983d5de95b95b10a858c19edccaad1
wangchunsheng <34767187+yii-diandi@users.noreply.github.com>
2022-08-08 16:42:06 +0800

    修复插件安装与卸载代码生成器错误

--- console/gii/giitpl/addons/default/install.php
+++ console/gii/giitpl/addons/default/install.php
@@ -4,30 +4,21 @@
  * @Author: Wang Chunsheng 2192138785@qq.com
  * @Date:   2020-03-26 00:09:42
  * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2021-01-01 01:52:31
+ * @Last Modified time: 2022-08-08 16:36:25
  */
 echo "<?php\n";
 ?>
 
 namespace <?= $generator->getControllerNamespace(); ?>;
 
-use Yii;
-use yii\db\Migration;
-use common\helpers\MigrateHelper;
-use common\interfaces\AddonWidget;
+use common\components\addons\AddonsInstall;
 
 /**
- * 安装
+ * 安装.
  *
  * Class Install
- * @package addons\Merchants
  */
-class Install extends Migration implements AddonWidget
+class Install extends AddonsInstall
 {
-  public function run($addon)
-  {
-    MigrateHelper::upByPath([
-      '@addons/<?= $generator->moduleID; ?>/migrations/'
-    ]);
-  }
+    public $addons = '<?= $generator->moduleID; ?>';
 }
--- console/gii/giitpl/addons/default/uninstall.php
+++ console/gii/giitpl/addons/default/uninstall.php
@@ -4,35 +4,22 @@
  * @Author: Wang Chunsheng 2192138785@qq.com
  * @Date:   2020-03-26 00:09:42
  * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2021-03-24 19:47:59
+ * @Last Modified time: 2022-08-08 16:38:26
  */
 echo "<?php\n";
 ?>
 
 namespace <?= $generator->getControllerNamespace(); ?>;
 
-use yii\db\Migration;
-use common\enums\AppEnum;
-use common\models\rbac\AuthItemChild;
-use common\models\rbac\AuthRole;
-use common\models\common\ConfigValue;
-use common\helpers\MigrateHelper;
-use common\interfaces\AddonWidget;
+use common\components\addons\AddonsUninstall;
+
 
 /**
- * 卸载
+ * 卸载.
  *
- * Class UnInstall
- * @package addons\Merchants
+ * Class Install
  */
-class UnInstall extends Migration implements AddonWidget
+class uninstall extends AddonsUninstall
 {
-    
-    public function run($addon)
-    {
-
-        MigrateHelper::downByPath([
-            '@addons/<?= $generator->moduleID; ?>/migrations/'
-        ]);
-    }
+    public $addons = '<?= $generator->moduleID; ?>';
 }
