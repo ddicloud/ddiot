15c471ffc8cad5718450cf1402c8335795ac3b6d
wangchunsheng <34767187+yii-diandi@users.noreply.github.com>
2022-08-12 13:42:11 +0800

    配置文件路径

--- common/components/swoole/TcpServerController.php
+++ common/components/swoole/TcpServerController.php
@@ -3,7 +3,7 @@
  * @Author: Wang chunsheng  email:2192138785@qq.com
  * @Date:   2022-06-05 10:04:24
  * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2022-08-12 13:38:02
+ * @Last Modified time: 2022-08-12 13:41:13
  */
 
 namespace common\components\swoole;
@@ -38,6 +38,7 @@ class TcpServerController extends BaseController implements SwooleServer
     {
         parent::init();
         $confPath = Yii::getAlias('@addons/'.$this->addons.'config/swoole/base.php');
+        echo  '路径：'.$confPath;
         if(file_exists($confPath)){
             $config = require Yii::getAlias('@dddons/'.$this->addons.'config/swoole/base.php');
             $this->config =  yii\helpers\ArrayHelper::merge(
