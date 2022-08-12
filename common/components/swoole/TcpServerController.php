ec9824c0dc5065dc89164c1f9a82f6ebf4c65872
wangchunsheng <34767187+yii-diandi@users.noreply.github.com>
2022-08-12 14:37:15 +0800

    tcp服务封装

--- common/components/swoole/TcpServerController.php
+++ common/components/swoole/TcpServerController.php
@@ -3,7 +3,7 @@
  * @Author: Wang chunsheng  email:2192138785@qq.com
  * @Date:   2022-06-05 10:04:24
  * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2022-08-12 14:31:22
+ * @Last Modified time: 2022-08-12 14:37:05
  */
 
 namespace common\components\swoole;
@@ -49,7 +49,7 @@ class TcpServerController extends BaseController implements SwooleServer
                         require(__DIR__ . '/../../config/params-local.php'),
                     ]
                 ],
-                require Yii::getAlias('@swooleService\config\web.php'),
+                require Yii::getAlias('@swooleService/config/web.php'),
             );
             $this->config =  yii\helpers\ArrayHelper::merge(
                 [
