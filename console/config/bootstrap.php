4969e7e0040bd15242f5c65b69d26c4fdd26591b
wangchunsheng <34767187+yii-diandi@users.noreply.github.com>
2022-08-18 13:32:38 +0800

    1

--- console/config/bootstrap.php
+++ console/config/bootstrap.php
@@ -3,6 +3,5 @@
  * @Author: Wang chunsheng  email:2192138785@qq.com
  * @Date:   2020-10-27 14:38:49
  * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2022-08-18 12:20:00
+ * @Last Modified time: 2022-08-18 13:31:56
  */
-// require_once dirname(dirname(__DIR__)).'/swoole/vendor/autoload.php';
