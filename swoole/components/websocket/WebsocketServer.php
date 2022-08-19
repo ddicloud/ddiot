742b2803b5acebac0c4595aa1edd43ccd05163dd
wangchunsheng <34767187+yii-diandi@users.noreply.github.com>
2022-08-19 11:03:09 +0800

    handshake握手协议处理

--- swoole/components/websocket/WebsocketServer.php
+++ swoole/components/websocket/WebsocketServer.php
@@ -3,7 +3,7 @@
  * @Author: Wang chunsheng  email:2192138785@qq.com
  * @Date:   2022-08-17 09:25:45
  * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2022-08-18 12:00:20
+ * @Last Modified time: 2022-08-19 11:00:28
  */
 
 namespace swooleService\components\websocket;
@@ -12,4 +12,43 @@ use diandi\swoole\websocket\server\WebSocketServer as ServerWebSocketServer;
 
 class WebsocketServer extends ServerWebSocketServer
 {
+    public function onHandshake(\Swoole\Http\Request $request, \Swoole\Http\Response $response)
+    {
+        // websocket握手连接算法验证
+        $secWebSocketKey = $request->header['sec-websocket-key'];
+        $patten = '#^[+/0-9A-Za-z]{21}[AQgw]==$#';
+        if (0 === preg_match($patten, $secWebSocketKey) || 16 !== strlen(base64_decode($secWebSocketKey))) {
+            $response->end();
+
+            return false;
+        }
+        echo $request->header['sec-websocket-key'];
+        $key = base64_encode(
+            sha1(
+                $request->header['sec-websocket-key'].'258EAFA5-E914-47DA-95CA-C5AB0DC85B11',
+                true
+            )
+        );
+
+        $headers = [
+            'Upgrade' => 'websocket',
+            'Connection' => 'Upgrade',
+            'Sec-WebSocket-Accept' => $key,
+            'Sec-WebSocket-Version' => '13',
+        ];
+
+        // WebSocket connection to 'ws://127.0.0.1:9502/'
+        // failed: Error during WebSocket handshake:
+        // Response must not include 'Sec-WebSocket-Protocol' header if not present in request: websocket
+        if (isset($request->header['sec-websocket-protocol'])) {
+            $headers['Sec-WebSocket-Protocol'] = $request->header['sec-websocket-protocol'];
+        }
+
+        foreach ($headers as $key => $val) {
+            $response->header($key, $val);
+        }
+
+        $response->status(101);
+        $response->end();
+    }
 }
