6d5a49e2bc3ee6e1a7aebfdcbb3a5cf47e96eda4
wangchunsheng <34767187+yii-diandi@users.noreply.github.com>
2022-08-28 22:28:22 +0800

    入口处配置debug控制台

--- frontend/admin/index.php
+++ frontend/admin/index.php
@@ -4,13 +4,14 @@
  * @Author: Wang chunsheng  email:2192138785@qq.com
  * @Date:   2020-05-15 20:27:36
  * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2022-07-13 15:29:37
+ * @Last Modified time: 2022-08-28 22:26:51
  */
 if (in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1'])) {
     //开发环境dev的入口文件代码
     defined('YII_DEBUG') or define('YII_DEBUG', true);
     defined('YII_ENV') or define('YII_ENV', 'dev');
 } else {
+    defined('YII_ENV_DEV') or define('YII_ENV_DEV',false);
     //生产环境prod的入口文件代码
     defined('YII_DEBUG') or define('YII_DEBUG', false);
     defined('YII_ENV') or define('YII_ENV', 'prod');
