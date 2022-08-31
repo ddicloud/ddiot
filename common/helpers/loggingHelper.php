e7c5f1aa6864db580f2e5513fce8a3e28bf6f1f4
wangchunsheng <34767187+yii-diandi@users.noreply.github.com>
2022-08-31 19:52:33 +0800

    日志写入取消协程化处理

--- common/helpers/loggingHelper.php
+++ common/helpers/loggingHelper.php
@@ -3,7 +3,7 @@
  * @Author: Wang chunsheng  email:2192138785@qq.com
  * @Date:   2020-06-27 14:06:58
  * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2022-08-31 19:46:51
+ * @Last Modified time: 2022-08-31 19:52:19
  */
 
 namespace common\helpers;
@@ -56,18 +56,11 @@ class loggingHelper
                 $contentTxt = $content;
             }
 
-            if ($appId != 'app-swoole') {
-                Yii::$app->log->targets[0]->logFile = $basepath;
-                Yii::$app->log->targets[0]->maxFileSize = 2000;
-                Yii::$app->log->targets[0]->maxLogFiles = 10;
-                //在需要记录日志的地方先赋值log文件地址：
-                return Yii::info($contentTxt, 'ddicms');
-            } else {
-                $filename = $basepath;
-                go(function () use ($filename, $time, $mark, $contentTxt) {
-                    $w = \Swoole\Coroutine\System::writeFile($filename, "\r\n" . $time . '-' . $mark . ':' . $contentTxt, FILE_APPEND);
-                });
-            }
+            Yii::$app->log->targets[0]->logFile = $basepath;
+            Yii::$app->log->targets[0]->maxFileSize = 2000;
+            Yii::$app->log->targets[0]->maxLogFiles = 10;
+            //在需要记录日志的地方先赋值log文件地址：
+            return Yii::info($contentTxt, 'ddicms');
         }
 
     }
