b669e370db2e39fb2aba5f309a535140a733aa45
wangchunsheng <34767187+yii-diandi@users.noreply.github.com>
2022-08-31 23:54:21 +0800

    swoole sql转义处理

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -4,15 +4,14 @@
  * @Author: Wang Chunsheng 2192138785@qq.com
  * @Date:   2020-03-22 17:22:31
  * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2022-08-18 11:42:32
+ * @Last Modified time: 2022-08-31 21:32:23
  */
 Yii::setAlias('@common', dirname(__DIR__));
-Yii::setAlias('@frontend', dirname(dirname(__DIR__)).'/frontend');
-Yii::setAlias('@backend', dirname(dirname(__DIR__)).'/backend');
-Yii::setAlias('@console', dirname(dirname(__DIR__)).'/console');
-Yii::setAlias('@api', dirname(dirname(__DIR__)).'/api');
-Yii::setAlias('@admin', dirname(dirname(__DIR__)).'/admin');
-Yii::setAlias('@addons', dirname(dirname(__DIR__)).'/addons');
-Yii::setAlias('@attachment', dirname(dirname(__DIR__)).'/frontend/attachment');
-Yii::setAlias('@swooleService', dirname(dirname(__DIR__)).'/swoole');
-Yii::setAlias('@vue', dirname(dirname(__DIR__)).'/common/widgets/firevue');
+Yii::setAlias('@frontend', dirname(dirname(__DIR__)) . '/frontend');
+Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
+Yii::setAlias('@api', dirname(dirname(__DIR__)) . '/api');
+Yii::setAlias('@admin', dirname(dirname(__DIR__)) . '/admin');
+Yii::setAlias('@addons', dirname(dirname(__DIR__)) . '/addons');
+Yii::setAlias('@attachment', dirname(dirname(__DIR__)) . '/frontend/attachment');
+Yii::setAlias('@swooleService', dirname(dirname(__DIR__)) . '/swoole');
+Yii::setAlias('@vue', dirname(dirname(__DIR__)) . '/common/widgets/firevue');
