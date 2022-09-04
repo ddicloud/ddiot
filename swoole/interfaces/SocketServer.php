5b5c22ac7c19b9b82fa5c7301ae2f5ecec603127
lijinfang <1765997546@qq.com>
2022-09-04 09:09:16 +0800

    websoketbug修复

--- swoole/interfaces/SocketServer.php
+++ swoole/interfaces/SocketServer.php
@@ -3,13 +3,13 @@
  * @Author: Wang chunsheng  email:2192138785@qq.com
  * @Date:   2021-12-11 00:34:05
  * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2022-09-03 08:17:42
+ * @Last Modified time: 2022-09-04 09:09:04
  */
 
 namespace ddswoole\interfaces;
+
 use Swoole\Http\Request;
 use Swoole\Http\Response;
-
 
 /**
  * swoole服务
@@ -18,20 +18,19 @@ use Swoole\Http\Response;
  */
 interface SocketServer
 {
-    
-    public function handles(Request $request, Response $ws);
-
     public function addlistenerPort($channel);
 
     /**
-     * 响应消息处理
-     * @param Type|null $var
+     * 响应消息处理.
+     *
      * @return void
      * @date 2022-09-02
+     *
      * @example
+     *
      * @author Wang Chunsheng
+     *
      * @since
      */
-    public function messageReturn(Request $request, Response $ws,$message,$channel);
-
+    public function messageReturn(Request $request, Response $ws, $message, $channel);
 }
