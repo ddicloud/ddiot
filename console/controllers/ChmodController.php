e94a3491c2240e9661e2e691b3615039f9325d5b
wangchunsheng <34767187+yii-diandi@users.noreply.github.com>
2022-09-06 07:16:03 +0800

    文件夹权限初始化bug处理

--- console/controllers/ChmodController.php
+++ console/controllers/ChmodController.php
@@ -4,7 +4,7 @@
  * @Author: Wang chunsheng  email:2192138785@qq.com
  * @Date:   2020-07-02 12:49:11
  * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2022-08-24 19:31:44
+ * @Last Modified time: 2022-09-06 07:15:49
  */
 
 namespace console\controllers;
@@ -18,57 +18,57 @@ class ChmodController extends \yii\console\Controller
 {
     public function actionIndex()
     {
-        $baseDir = dirname(__FILE__).'/../../';
+        $baseDir = dirname(__FILE__) . '/../../';
 
-        $dirs = ['api/runtime/', 'frontend/runtime/', 'frontend/assets/', 'frontend/attachment', 'swoole/runtime/', 'admin/runtime/', 'api/web/store/'];
+        $dirs = ['api/runtime/', 'frontend/runtime/', 'frontend/assets/', 'frontend/attachment', 'console/runtime/', 'swoole/runtime/', 'admin/runtime/', 'api/web/store/'];
 
         foreach ($dirs as $key => $value) {
-            if (is_dir($baseDir.$value)) {
-                chmod($baseDir.$value, 0777);
+            if (is_dir($baseDir . $value)) {
+                chmod($baseDir . $value, 0777);
             } else {
-                mkdir($baseDir.$value, 0777);
-                chmod($baseDir.$value, 0777);
+                mkdir($baseDir . $value, 0777);
+                chmod($baseDir . $value, 0777);
             }
-            echo '目录'.$value.'权限设置成功'.PHP_EOL;
+            echo '目录' . $value . '权限设置成功' . PHP_EOL;
             sleep(1);
         }
     }
 
     public function actionClear()
     {
-        $baseDir = dirname(__FILE__).'/../../';
+        $baseDir = dirname(__FILE__) . '/../../';
 
         $dirs = ['api/runtime', 'console/runtime', 'swoole/runtime', 'admin/runtime'];
 
         foreach ($dirs as $key => $value) {
-            $logs = FileHelper::file_lists($baseDir.$value,1,'log');
-            $datas = FileHelper::file_lists($baseDir.$value,1,'data');
+            $logs = FileHelper::file_lists($baseDir . $value, 1, 'log');
+            $datas = FileHelper::file_lists($baseDir . $value, 1, 'data');
 
-            $lists = array_merge($logs,$datas);
+            $lists = array_merge($logs, $datas);
             foreach ($lists as $k => $file) {
-                echo '准备清理文件'.$file.PHP_EOL;
+                echo '准备清理文件' . $file . PHP_EOL;
                 FileHelper::file_delete($file);
             }
-            if (is_dir($baseDir.$value)) {
-                echo '准备清理目录'.$baseDir.$value.PHP_EOL;
-                FileHelper::rmdirs($baseDir.$value,true);
+            if (is_dir($baseDir . $value)) {
+                echo '准备清理目录' . $baseDir . $value . PHP_EOL;
+                FileHelper::rmdirs($baseDir . $value, true);
             }
             ob_end_clean();
         }
 
         // 重新修复文件权限
-        
-        $baseDir = dirname(__FILE__).'/../../';
+
+        $baseDir = dirname(__FILE__) . '/../../';
         $dirs = ['api/runtime/', 'frontend/runtime/', 'frontend/assets/', 'frontend/attachment', 'swoole/runtime/', 'admin/runtime/', 'api/web/store/'];
 
         foreach ($dirs as $key => $value) {
-            if (is_dir($baseDir.$value)) {
-                chmod($baseDir.$value, 0777);
+            if (is_dir($baseDir . $value)) {
+                chmod($baseDir . $value, 0777);
             } else {
-                mkdir($baseDir.$value, 0777);
-                chmod($baseDir.$value, 0777);
+                mkdir($baseDir . $value, 0777);
+                chmod($baseDir . $value, 0777);
             }
-            echo '目录'.$value.'权限设置成功'.PHP_EOL;
+            echo '目录' . $value . '权限设置成功' . PHP_EOL;
             sleep(1);
         }
     }
