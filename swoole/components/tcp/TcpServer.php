b50b11bafcc953218b256c6ecc924a12fa9934e6
wangchunsheng <34767187+yii-diandi@users.noreply.github.com>
2022-09-07 00:23:42 +0800

    tcp服务处理

--- swoole/components/tcp/TcpServer.php
+++ swoole/components/tcp/TcpServer.php
@@ -3,13 +3,13 @@
  * @Author: Wang chunsheng  email:2192138785@qq.com
  * @Date:   2022-08-17 09:25:45
  * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2022-08-18 12:00:13
+ * @Last Modified time: 2022-09-07 00:23:28
  */
 
 namespace ddswoole\components\tcp;
 
-use diandi\swoole\server\BaseServer;
+use diandi\swoole\tcp\server\TcpServer as ServerTcpServer;
 
-class TcpServer extends BaseServer
+class TcpServer extends ServerTcpServer
 {
 }
