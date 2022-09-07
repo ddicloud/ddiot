3641c76656c81d531b50938813e605fcf5f52096
wangchunsheng <34767187+yii-diandi@users.noreply.github.com>
2022-09-07 11:14:18 +0800

    tcp服务处理

--- swoole/components/tcp/TcpServer.php
+++ swoole/components/tcp/TcpServer.php
@@ -3,7 +3,7 @@
  * @Author: Wang chunsheng  email:2192138785@qq.com
  * @Date:   2022-08-17 09:25:45
  * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2022-09-07 09:48:36
+ * @Last Modified time: 2022-09-07 11:14:07
  */
 
 namespace ddswoole\components\tcp;
@@ -23,10 +23,6 @@ class TcpServer extends ServerTcpServer
     private $application;
 
     private $config;
-
-    public $pools;
-
-    public $ProcessNum = 1;
 
     /**
      * 重新实例化application.
@@ -54,7 +50,6 @@ class TcpServer extends ServerTcpServer
         if (!empty($this->tables) && is_array($this->tables)) {
             $this->prepareTables($this->tables);
         }
-        $this->poolStart();
     }
 
     /**
