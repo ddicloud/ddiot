179fef5b2006ad9a47dfda86ddf593c3a4b743d6
wangchunsheng <34767187+yii-diandi@users.noreply.github.com>
2022-09-08 20:36:54 +0800

    增加控制台信息输出助手函数

--- swoole/servers/DebugService.php
+++ swoole/servers/DebugService.php
@@ -3,7 +3,7 @@
  * @Author: Wang chunsheng  email:2192138785@qq.com
  * @Date:   2022-09-04 00:11:18
  * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2022-09-06 15:31:31
+ * @Last Modified time: 2022-09-08 20:36:37
  */
 
 namespace ddswoole\servers;
@@ -20,5 +20,38 @@ class DebugService extends BaseService
                 var_dump($row['file'].':'.$row['line'].'行,调用方法:'.$row['function']);
             }
         }
+    }
+
+   /**
+    * 控制台调试内容显示
+    * @param [type] $remark 备注
+    * @param [type] $content 结果信息
+    * @return void
+    * @date 2022-09-08
+    * @example
+    * @author Wang Chunsheng
+    * @since
+    */
+    public static function consoleWrite($remark,$content)
+    {
+        if(is_array($content)){
+            $content = json_encode($content);
+        }
+        print "#[$remark]#$content". PHP_EOL;
+    }
+
+    /**
+     * 控制台分割线
+     * @param [type] $remark
+     * @return void
+     * @date 2022-09-08
+     * @example
+     * @author Wang Chunsheng
+     * @since
+     */
+    public function consoleCrosswise($remark)
+    {
+        print "#[$remark]#-----------------------------------------". PHP_EOL;
+        
     }
 }
