efa6bab70acf3353d1583ce977e22e03b16242c6
wangchunsheng <34767187+yii-diandi@users.noreply.github.com>
2022-09-15 16:20:11 +0800

    注销封装到接口类

--- swoole/interfaces/servers/BaseInterfaceServer.php
+++ swoole/interfaces/servers/BaseInterfaceServer.php
@@ -3,10 +3,13 @@
  * @Author: Wang chunsheng  email:2192138785@qq.com
  * @Date:   2022-09-15 07:42:38
  * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2022-09-15 07:46:14
+ * @Last Modified time: 2022-09-15 16:19:00
  */
 
 namespace ddswoole\interfaces\servers;
+
+use Swoole\Http\Request;
+use Swoole\Http\Response;
 
 /**
  * swoole服务
@@ -16,4 +19,6 @@ namespace ddswoole\interfaces\servers;
 interface BaseInterfaceServer
 {
     public function run();
+
+    public function destory(Request $request, Response $ws);
 }
