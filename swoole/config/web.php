7ff2034e9074667d28d8cbd461759d3512e08ad0
wangchunsheng <34767187+yii-diandi@users.noreply.github.com>
2022-09-22 13:20:37 +0800

    关闭swoole数据库日志

--- swoole/config/web.php
+++ swoole/config/web.php
@@ -4,7 +4,7 @@
  * @Author: Wang chunsheng  email:2192138785@qq.com
  * @Date:   2021-01-19 20:27:34
  * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2022-09-14 19:47:27
+ * @Last Modified time: 2022-09-22 13:20:25
  */
 
 use diandi\swoole\web\ErrorHandler;
@@ -14,6 +14,8 @@ use diandi\swoole\web\Response;
 require __DIR__.'/../../common/config/bootstrap.php';
 $db = require __DIR__.'/../../common/config/db.php';
 $db['class'] = 'ddswoole\db\Connection';
+$db['enableLogging'] = false;
+$db['enableProfiling'] = false;
 
 return [
     'id' => 'app-ddswoole',
