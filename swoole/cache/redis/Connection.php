1eebab81141e7e0006fc7821477e1fcf23b5901a
wangchunsheng <34767187+yii-diandi@users.noreply.github.com>
2022-09-26 10:15:06 +0800

    2

--- swoole/cache/redis/Connection.php
+++ swoole/cache/redis/Connection.php
@@ -3,7 +3,7 @@
  * @Author: Wang chunsheng  email:2192138785@qq.com
  * @Date:   2022-09-24 11:56:17
  * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2022-09-26 10:12:47
+ * @Last Modified time: 2022-09-26 10:15:03
  */
 
 namespace ddswoole\cache\redis;
@@ -89,9 +89,9 @@ class Connection extends \yii\redis\Connection
         // backup the params for try again when execute fail
         try {
             \Yii::trace("Executing Redis Command: {$name}", __METHOD__);
-            print_r($this->_socket->getConnection());
+            print_r($this->_socket);
             print_r([$name, $params]);
-            $ret = $this->_socket->getConnection()->{$name}(...$params);
+            $ret = $this->_socket->{$name}(...$params);
             if ($this->_socket->errCode) {
                 throw new Exception("Redis error: {$this->_socket->errMsg} \nRedis command was: ".$name);
             }
