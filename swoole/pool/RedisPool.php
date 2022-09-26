a46e0aecfc54014a16ee97777209e9e65153d3cd
wangchunsheng <34767187+yii-diandi@users.noreply.github.com>
2022-09-26 10:45:35 +0800

    1

--- swoole/pool/RedisPool.php
+++ swoole/pool/RedisPool.php
@@ -3,7 +3,7 @@
  * @Author: Wang chunsheng  email:2192138785@qq.com
  * @Date:   2022-08-30 18:16:03
  * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2022-09-26 10:21:47
+ * @Last Modified time: 2022-09-26 10:45:28
  */
 
 namespace ddswoole\pool;
@@ -172,7 +172,7 @@ class RedisPool extends Component
         $connect = null;
         try {
             $connect = $this->getConnection();
-            print_r([$redisCommand, $params]);
+            print_r([$connect, $redisCommand, $params]);
             $res = $connect->{$redisCommand}(...$params);
             $resultData = new RedisResultData([
                 'result' => $res,
