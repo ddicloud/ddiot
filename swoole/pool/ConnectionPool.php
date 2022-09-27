b99f768ccc4f775473c98f8711593d211b24be77
wangchunsheng <34767187+yii-diandi@users.noreply.github.com>
2022-09-27 19:46:34 +0800

    1

--- swoole/pool/ConnectionPool.php
+++ swoole/pool/ConnectionPool.php
@@ -3,7 +3,7 @@
  * @Author: Wang chunsheng  email:2192138785@qq.com
  * @Date:   2022-08-30 16:43:08
  * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2022-09-27 19:42:40
+ * @Last Modified time: 2022-09-27 19:46:31
  */
 
 namespace ddswoole\pool;
@@ -54,6 +54,7 @@ abstract class ConnectionPool extends Component
         if (!$this->queue->isEmpty()) {
             $connect = $this->queue->shift();
         } elseif ($this->currentCount < $this->maxActive) {
+            $this->currentCount++;
             $connect = $this->createConnect();
         } elseif ($retry < 3) {
             //重试3次
@@ -64,7 +65,7 @@ abstract class ConnectionPool extends Component
         if ($connect === null) {
             throw new Exception('connection pool is full');
         }
-        $this->currentCount++;
+      
         return $connect;
     }
 
