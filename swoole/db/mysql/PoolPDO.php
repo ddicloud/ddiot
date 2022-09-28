c3a1da2b933c9bb4b51c6c5a4e165d174c8540a3
wangchunsheng <34767187+yii-diandi@users.noreply.github.com>
2022-09-28 08:50:06 +0800

    1

--- swoole/db/mysql/PoolPDO.php
+++ swoole/db/mysql/PoolPDO.php
@@ -3,7 +3,7 @@
  * @Author: Wang chunsheng  email:2192138785@qq.com
  * @Date:   2022-08-30 21:27:46
  * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2022-09-28 08:47:51
+ * @Last Modified time: 2022-09-28 08:50:01
  */
 
 namespace ddswoole\db\mysql;
@@ -164,7 +164,7 @@ class PoolPDO
         $cm->releaseConnection($this->poolKey, $this->client);
          // 释放连接
          $pool = $this->getConnectionFromPool();
-         print_r(get_class_methods($pool->getPools()));
+         print_r($pool->getPools());
          $pool->close($this->client);
         $this->client = null;
     }
