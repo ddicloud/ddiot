14cba4d2a21fb1461495aa5933d4c0f5bfb84ff8
Radish <minradish@163.com>
2022-10-14 15:21:35 +0800

    公众号支付修改
    (cherry picked from commit dcf5b2e524a11d17dc5de8b28c208707f5c7d9bd)

--- api/modules/officialaccount/controllers/BasicsController.php
+++ api/modules/officialaccount/controllers/BasicsController.php
@@ -3,8 +3,8 @@
 /**
  * @Author: Wang Chunsheng 2192138785@qq.com
  * @Date:   2020-03-09 01:32:28
- * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2022-07-29 15:23:00
+ * @Last Modified by:   Radish <minradish@163.com>
+ * @Last Modified time: 2022-10-14 15:19:53
  */
 
 namespace api\modules\officialaccount\controllers;
@@ -116,18 +116,18 @@ class BasicsController extends AController
         $code = $users['code'];
 
         unset($users['code']);
-        $logPath = Yii::getAlias('@runtime/wechat/signup/'.date('ymd').'.log');
+        $logPath = Yii::getAlias('@runtime/wechat/signup/' . date('ymd') . '.log');
 
-        FileHelper::writeLog($logPath, '登录日志：请求code的用户数据'.json_encode($users));
+        FileHelper::writeLog($logPath, '登录日志：请求code的用户数据' . json_encode($users));
 
         $wechat = Yii::$app->wechat->getApp();
 
         if (empty($users['openid'])) { // 当openid有值的时候为app登录
             $user = $wechat->auth->session($code);
-            FileHelper::writeLog($logPath, '登录日志：获取信息'.json_encode($user));
+            FileHelper::writeLog($logPath, '登录日志：获取信息' . json_encode($user));
 
             if (key_exists('errcode', $user)) {
-                FileHelper::writeLog($logPath, '登录日志：请求错误信息'.json_encode($user));
+                FileHelper::writeLog($logPath, '登录日志：请求错误信息' . json_encode($user));
 
                 return ResultHelper::json(401, $user['errmsg'], []);
             }
@@ -136,7 +136,7 @@ class BasicsController extends AController
         }
         // 查询该openID是否存在
         $res = Yii::$app->fans->signup($users);
-        FileHelper::writeLog($logPath, '登录成功：'.json_encode($res));
+        FileHelper::writeLog($logPath, '登录成功：' . json_encode($res));
 
         return ResultHelper::json(200, '登录成功', $res);
     }
@@ -168,16 +168,16 @@ class BasicsController extends AController
     public function actionAuth()
     {
         global $_GPC;
-        $logPath = Yii::getAlias('@runtime/wechat/auth/'.date('ymd').'.log');
+        $logPath = Yii::getAlias('@runtime/wechat/auth/' . date('ymd') . '.log');
 
         $redirect_uri = $_GPC['redirect_uri'];
         $route = $_GPC['route'];
 
         $wechat = Yii::$app->wechat->app;
         $response = $wechat->oauth->scopes(['snsapi_userinfo'])
-        ->redirect($redirect_uri);
+            ->redirect($redirect_uri);
 
-        FileHelper::writeLog($logPath, 'auth'.json_encode([$redirect_uri, $route, $response]));
+        FileHelper::writeLog($logPath, 'auth' . json_encode([$redirect_uri, $route, $response]));
 
         return ResultHelper::json(200, '授权成功', $response);
     }
@@ -202,7 +202,7 @@ class BasicsController extends AController
     public function actionUserinfo()
     {
         global $_GPC;
-        $logPath = Yii::getAlias('@runtime/officialaccount/signup/'.date('ymd').'.log');
+        $logPath = Yii::getAlias('@runtime/officialaccount/signup/' . date('ymd') . '.log');
         if (empty($_GPC['code'])) {
             return ResultHelper::json(400, 'code 参数不能为空', []);
         }
@@ -326,8 +326,8 @@ class BasicsController extends AController
             // 'open_id' => 'okFAZ0-',  //JS支付必填
             // 'auth_code' => 'ojPztwJ5bRWRt_Ipg',  刷卡支付必填
         ];
-        $logPath = Yii::getAlias('@runtime/wechat/payparameters'.date('ymd').'.log');
-        FileHelper::writeLog($logPath, '订单数据'.json_encode(ArrayHelper::toArray($orderData)));
+        $logPath = Yii::getAlias('@runtime/wechat/payparameters' . date('ymd') . '.log');
+        FileHelper::writeLog($logPath, '订单数据' . json_encode(ArrayHelper::toArray($orderData)));
 
         // 生成支付配置
         $payment = Yii::$app->wechat->payment;
@@ -336,6 +336,9 @@ class BasicsController extends AController
         if ($result['return_code'] == 'SUCCESS' && $result['result_code'] == 'SUCCESS') {
             $prepayId = $result['prepay_id'];
             $config = $payment->jssdk->sdkConfig($prepayId);
+            if ($orderData['trade_type'] == 'NATIVE') {
+                $config['code_url'] = $result['code_url'];
+            }
 
             return ResultHelper::json(200, '支付参数获取成功', $config);
         } else {
@@ -368,8 +371,8 @@ class BasicsController extends AController
             // 'open_id' => 'okFAZ0-',  //JS支付必填
             // 'auth_code' => 'ojPztwJ5bRWRt_Ipg',  刷卡支付必填
         ];
-        $logPath = Yii::getAlias('@runtime/wechat/payparameters'.date('ymd').'.log');
-        FileHelper::writeLog($logPath, '订单数据'.json_encode(ArrayHelper::toArray($orderData)));
+        $logPath = Yii::getAlias('@runtime/wechat/payparameters' . date('ymd') . '.log');
+        FileHelper::writeLog($logPath, '订单数据' . json_encode(ArrayHelper::toArray($orderData)));
 
         // 生成支付配置
         $payment = Yii::$app->wechat->payment;
@@ -381,7 +384,7 @@ class BasicsController extends AController
 
             return ResultHelper::json(200, '支付参数获取成功', $config);
         } else {
-            FileHelper::writeLog($logPath, 'app支付参数获取错误'.json_encode($result));
+            FileHelper::writeLog($logPath, 'app支付参数获取错误' . json_encode($result));
 
             return ResultHelper::json(401, $result['err_code_des'], $result);
         }
@@ -392,32 +395,32 @@ class BasicsController extends AController
      */
     public function actionNotify()
     {
-        $logPath = Yii::getAlias('@runtime/wechat/notify/'.date('ymd').'.log');
-        FileHelper::writeLog($logPath, '开始回调'.json_encode(Yii::$app->wechat->payment));
+        $logPath = Yii::getAlias('@runtime/wechat/notify/' . date('ymd') . '.log');
+        FileHelper::writeLog($logPath, '开始回调' . json_encode(Yii::$app->wechat->payment));
 
         $input = file_get_contents('php://input');
 
-        FileHelper::writeLog($logPath, '开始回调回去到的内容'.json_encode($input));
+        FileHelper::writeLog($logPath, '开始回调回去到的内容' . json_encode($input));
 
         $response = Yii::$app->wechat->payment->handlePaidNotify(function ($message, $fail) {
-            $logPath = Yii::getAlias('@runtime/wechat/notify/'.date('ymd').'.log');
+            $logPath = Yii::getAlias('@runtime/wechat/notify/' . date('ymd') . '.log');
 
             FileHelper::writeLog($logPath, Json::encode(ArrayHelper::toArray($message)));
             /////////////  建议在这里调用微信的【订单查询】接口查一下该笔订单的情况，确认是已经支付 /////////////
 
             // return_code 表示通信状态，不代表支付状态
             if ($message['return_code'] === 'SUCCESS') {
-                FileHelper::writeLog($logPath, '成功回调'.$message['out_trade_no']);
+                FileHelper::writeLog($logPath, '成功回调' . $message['out_trade_no']);
                 $orderInfo = DdCorePaylog::findOne(['uniontid' => $message['out_trade_no']]);
-                FileHelper::writeLog($logPath, '订单信息'.json_encode($orderInfo));
+                FileHelper::writeLog($logPath, '订单信息' . json_encode($orderInfo));
 
                 $module = $orderInfo['module'];
 
-                FileHelper::writeLog($logPath, '下单模块'.$module);
+                FileHelper::writeLog($logPath, '下单模块' . $module);
 
                 $notify = Yii::$app->getModule($module)->Notify($message);
 
-                FileHelper::writeLog($logPath, '下单模块处理数据结构'.json_decode($notify));
+                FileHelper::writeLog($logPath, '下单模块处理数据结构' . json_decode($notify));
 
                 if ($notify) {
                     return true;
@@ -426,7 +429,7 @@ class BasicsController extends AController
 
             return $fail('处理失败，请稍后再通知我');
         });
-        FileHelper::writeLog($logPath, '回调完毕'.json_encode($response));
+        FileHelper::writeLog($logPath, '回调完毕' . json_encode($response));
 
         $response->send();
     }
