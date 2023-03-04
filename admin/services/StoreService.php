642fdacbeb98a155c93d17081b73cd5a39a51cbd
wangchunsheng <34767187+yii-diandi@users.noreply.github.com>
2023-03-04 17:20:07 +0800

    商户授权检索数据修复

--- admin/services/StoreService.php
+++ admin/services/StoreService.php
@@ -3,7 +3,7 @@
  * @Author: Wang chunsheng  email:2192138785@qq.com
  * @Date:   2022-10-26 15:43:38
  * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2023-03-04 17:14:22
+ * @Last Modified time: 2023-03-04 17:19:56
  */
 
 namespace admin\services;
@@ -435,8 +435,8 @@ class StoreService extends BaseService
             if($value['store']){
                 $lists[$value['store_id']] = [
                     'id'=>$value['store_id'],
-                    'name'=>$value['store']['business_name'],
-                    "label" => $value['store']['business_name'],
+                    'name'=>$value['store']['name'],
+                    "label" => $value['store']['name'],
                     "value" => $value['store_id'],
                 ];
             }
