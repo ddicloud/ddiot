a87dbde4874cde864a0cfbee57fba4497800d181
wangchunsheng <34767187+yii-diandi@users.noreply.github.com>
2023-03-04 20:00:06 +0800

    商户授权数据兼容处理

--- admin/services/StoreService.php
+++ admin/services/StoreService.php
@@ -3,7 +3,7 @@
  * @Author: Wang chunsheng  email:2192138785@qq.com
  * @Date:   2022-10-26 15:43:38
  * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2023-03-04 17:19:56
+ * @Last Modified time: 2023-03-04 19:59:48
  */
 
 namespace admin\services;
@@ -410,6 +410,7 @@ class StoreService extends BaseService
                 $lists[$value['bloc_id']] = [
                     'id'=>$value['bloc_id'],
                     'name'=>$value['bloc']['business_name'],
+                    'text'=>$value['bloc']['business_name'],
                     "label" => $value['bloc']['business_name'],
                     "value" => $value['bloc_id'],
                 ];
@@ -436,6 +437,7 @@ class StoreService extends BaseService
                 $lists[$value['store_id']] = [
                     'id'=>$value['store_id'],
                     'name'=>$value['store']['name'],
+                    'text'=>$value['store']['name'],
                     "label" => $value['store']['name'],
                     "value" => $value['store_id'],
                 ];
