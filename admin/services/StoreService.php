d6e1af7445d3c4d1afd01d9f2c0362f149cc3d12
wangchunsheng <34767187+yii-diandi@users.noreply.github.com>
2023-03-04 13:32:10 +0800

    商户授权数据格式调整

--- admin/services/StoreService.php
+++ admin/services/StoreService.php
@@ -3,7 +3,7 @@
  * @Author: Wang chunsheng  email:2192138785@qq.com
  * @Date:   2022-10-26 15:43:38
  * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2023-03-04 13:30:12
+ * @Last Modified time: 2023-03-04 13:32:00
  */
 
 namespace admin\services;
@@ -372,6 +372,8 @@ class StoreService extends BaseService
                     "label" => $value['store']['name'],
                     "value" => $value['store']['store_id'],
                 ];
+            } else {
+                unset($blocs[$value['bloc_id']]);
             }
         }
 
