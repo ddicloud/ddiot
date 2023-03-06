70377b6928f4a2523406301fecd06b634fd03608
wangchunsheng <34767187+yii-diandi@users.noreply.github.com>
2023-03-06 21:58:13 +0800

    增加blocs字段解析

--- common/behaviors/SaveBehavior.php
+++ common/behaviors/SaveBehavior.php
@@ -4,13 +4,12 @@
  * @Author: Wang chunsheng  email:2192138785@qq.com
  * @Date:   2020-05-15 22:50:42
  * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2022-10-28 14:34:05
+ * @Last Modified time: 2023-03-06 21:57:57
  */
 
 namespace common\behaviors;
 
 use admin\models\addons\models\Bloc as ModelsBloc;
-use ddswoole\servers\DebugService;
 use Yii;
 use yii\base\Behavior;
 use yii\db\BaseActiveRecord;
@@ -83,6 +82,13 @@ class SaveBehavior extends Behavior
 
         $time = $this->time_type === 'init' ? time() : date('Y-m-d H:i:s', time());
 
+        // 表单中提交了公司与商户就解析使用
+
+        $blocs = $_GPC['blocs'];
+        if (is_array($blocs)) {
+            $bloc_id = $blocs[0];
+            $store_id = $blocs[1];
+        }
         if ($this->value) {
             $time = $this->value;
         }
@@ -96,7 +102,7 @@ class SaveBehavior extends Behavior
             $this->adminAttribute => (int) $admin_id,
             $this->globalBlocAttribute => Yii::$app->params['global_bloc_id'] ?? 0,
         ];
-        unset($time,$bloc_id,$store_id,$admin_id,$blocPid,$_GPC);
+        unset($time, $bloc_id, $store_id, $admin_id, $blocPid, $_GPC);
         // DebugService::consoleWrite('行为-内存测试4');
     }
 
@@ -139,6 +145,6 @@ class SaveBehavior extends Behavior
     public function __destruct()
     {
         global $_GPC;
-        unset($_GPC,$this->_map,$this->attributes,$this->owner);
+        unset($_GPC, $this->_map, $this->attributes, $this->owner);
     }
 }
