ee4c813a5053364ee38e62a5f52f8e44d88028db
wangchunsheng <34767187+yii-diandi@users.noreply.github.com>
2023-03-15 14:38:23 +0800

    1

--- admin/services/StoreService.php
+++ admin/services/StoreService.php
@@ -3,7 +3,7 @@
  * @Author: Wang chunsheng  email:2192138785@qq.com
  * @Date:   2022-10-26 15:43:38
  * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2023-03-14 17:09:16
+ * @Last Modified time: 2023-03-15 14:37:35
  */
 
 namespace admin\services;
@@ -627,5 +627,18 @@ class StoreService extends BaseService
         }
 
         return true;
+    }
+
+    public static function deleteStore($store_id)
+    {
+        // 删除全局商户
+        BlocStore::deleteAll([
+            'store_id'=>$store_id
+        ]);
+        // 删除商户授权
+        UserStore::deleteAll([
+            'store_id'=>$store_id,
+            'user_id'=>Yii::$app->user->identity->user_id
+        ]);
     }
 }
