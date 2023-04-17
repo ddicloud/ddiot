d95f56c55b90077f4439c8491b2b995d19b8e2fb
wangchunsheng <34767187+yii-diandi@users.noreply.github.com>
2023-04-17 16:26:34 +0800

    2

--- common/components/Job.php
+++ common/components/Job.php
@@ -1,9 +1,10 @@
 <?php
+
 /**
  * @Author: Wang chunsheng  email:2192138785@qq.com
  * @Date:   2020-09-24 17:36:20
  * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2022-07-11 18:50:51
+ * @Last Modified time: 2023-04-17 16:26:23
  */
 
 namespace common\components;
@@ -16,30 +17,31 @@ use yii\queue\cli\LoopInterface;
 class Job extends BaseObject implements \yii\queue\JobInterface
 {
     public $bloc_id;
-    
+
     public $store_id;
-    
+
     public $addons;
-    
+
     public function init()
     {
         parent::init();
         Yii::$app->params['bloc_id'] = $this->bloc_id;
         Yii::$app->params['store_id'] = $this->store_id;
-        loggingHelper::writeLog('components','job','数据初始化',[
-            'bloc_id'=>$this->bloc_id,
-            'store_id'=>$this->store_id,
-            'addons'=>$this->addons
+        loggingHelper::writeLog('components', 'job', '数据初始化', [
+            'bloc_id' => $this->bloc_id,
+            'store_id' => $this->store_id,
+            'addons' => $this->addons
         ]);
-        Yii::$app->service->commonGlobalsService->initId($this->bloc_id, $this->store_id,$this->addons);
-        loggingHelper::writeLog('components','job','数据初始化获取',Yii::$app->service->commonGlobalsService->getConf($this->bloc_id));
+        Yii::$app->service->commonGlobalsService->initId($this->bloc_id, $this->store_id, $this->addons);
+        Yii::$app->service->commonGlobalsService->getConf($this->bloc_id);
+        loggingHelper::writeLog('components', 'job', '数据初始化获取', Yii::$app->service->commonGlobalsService->getConf($this->bloc_id));
     }
-    
-     /**
+
+    /**
      * @param Queue $queue which pushed and is handling the job
      * @return void|mixed result of the job execution
      */
-    public function execute($queue){
-     
+    public function execute($queue)
+    {
     }
 }
