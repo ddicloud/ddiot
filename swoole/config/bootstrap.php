e27a68fa9ca02750b60ea78fd7c2ce7e01293a6b
wangchunsheng <34767187+yii-diandi@users.noreply.github.com>
2023-04-21 09:02:04 +0800

    移除swoole，为接入easyswoole做准备

--- swoole/config/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @Author: Wang chunsheng  email:2192138785@qq.com
- * @Date:   2022-09-01 16:16:07
- * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2023-01-30 14:13:09
- */
-Yii::setAlias('@Simps', dirname(__DIR__) . '/vendor/simps/mqtt/src');
-Yii::setAlias('@ddswoole', dirname(dirname(__DIR__)) . '/swoole');
