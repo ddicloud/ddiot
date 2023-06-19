76771849c8049fd4a03fd91ebde85ffc66bb4998
wangchunsheng <34767187+yii-diandi@users.noreply.github.com>
2023-06-19 10:55:19 +0800

    12

--- common/traits/ActiveQuery/StoreTrait.php
+++ common/traits/ActiveQuery/StoreTrait.php
@@ -3,16 +3,31 @@
  * @Author: Wang chunsheng  email:2192138785@qq.com
  * @Date:   2022-04-22 14:40:19
  * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2022-04-22 15:03:10
+ * @Last Modified time: 2023-06-19 10:55:16
  */
 
 namespace common\traits\ActiveQuery;
 
 trait StoreTrait
 {
+    public $blocs = [];
+
     /** * {@inheritdoc} * @return CommentQuery */
     public static function find()
     {
         return new CommonQuery(get_called_class());
+    }
+
+    public function afterFind()
+    {
+        // 全局处理多商户数据返回
+        $this->blocs = [
+            $this->bloc_id,
+            $this->store_id
+        ];
+
+        // 其他处理代码
+
+        parent::afterFind();
     }
 }
