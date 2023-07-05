77e9b9490d0b7985ece003bd7038060c80bbed5f
wangchunsheng <34767187+yii-diandi@users.noreply.github.com>
2023-07-05 10:25:45 +0800

    http服务修复

--- api/controllers/SwooleController.php
+++ api/controllers/SwooleController.php
@@ -4,18 +4,18 @@
  * @Author: Wang chunsheng  email:2192138785@qq.com
  * @Date:   2023-07-05 10:20:44
  * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2023-07-05 10:21:40
+ * @Last Modified time: 2023-07-05 10:24:50
  */
 
 namespace api\controllers;
- 
+
 use easyswoole\Http\Request;
 use easyswoole\Http\Response;
 use EasySwoole\Http\AbstractInterface\Controller;
 
 class SwooleController extends Controller
 {
-    public function index(Request $request, Response $response)
+    public function index()
     {
         $response->write("Hello from EasySwoole!");
     }
