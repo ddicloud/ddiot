84f85e968810f93ad5c86d6cfe71469ffeda63dd
wangchunsheng <34767187+yii-diandi@users.noreply.github.com>
2023-07-11 13:22:52 +0800

    解密接口修复

--- api/modules/wechat/controllers/DecryptController.php
+++ api/modules/wechat/controllers/DecryptController.php
@@ -5,7 +5,7 @@
  * @Author: Wang chunsheng  email:2192138785@qq.com
  * @Date:   2023-07-11 13:04:33
  * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2023-07-11 13:11:53
+ * @Last Modified time: 2023-07-11 13:22:41
  */
 
 namespace api\modules\wechat\controllers;
@@ -16,6 +16,8 @@ use common\helpers\ResultHelper;
 
 class DecryptController extends AController
 {
+    protected $authOptional = ['msg'];
+
     public function actionMsg()
     {
         global $_GPC;
