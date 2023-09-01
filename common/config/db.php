471133ee12967dad84d403dd2047a6199a2470f7
wangchunsheng <34767187+yii-diandi@users.noreply.github.com>
2023-09-01 11:35:39 +0800

    详情查询修复处理

--- common/config/db.php
+++ common/config/db.php
@@ -21,7 +21,6 @@ return [
     'enableSchemaCache' => true,
     // Duration of schema cache.
     'schemaCacheDuration' => 3600,
-
     // Name of the cache component used to store schema information
     'schemaCache' => 'cache',
     'slaveConfig' => [
