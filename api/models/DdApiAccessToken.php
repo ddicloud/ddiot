b633a6fcbc0053367a3d8ffb58b5b26b90bc6cdc
wangchunsheng <34767187+yii-diandi@users.noreply.github.com>
2023-09-04 18:19:54 +0800

    兼容php8

--- api/models/DdApiAccessToken.php
+++ api/models/DdApiAccessToken.php
@@ -37,7 +37,7 @@ class DdApiAccessToken extends ActiveRecord implements IdentityInterface, RateLi
     const STATUS_ACTIVE = 0; //正常
 
     // 次数限制
-    public int $rateLimit;
+    public int $rateLimit = 60;
 
     // 时间范围
     public int $timeLimit = 60;
