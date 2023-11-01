33670b3a9ad1088acb184cbc06a912eb669b5ed8
wangchunsheng <34767187+yii-diandi@users.noreply.github.com>
2023-11-02 00:56:34 +0800

    放弃全局参数$_GPC的使用，兼容php8

--- common/components/ExtendedRequest.php
+++ common/components/ExtendedRequest.php
@@ -3,6 +3,7 @@
 namespace common\components;
 
 
+use Yii;
 use yii\web\Request;
 
 class ExtendedRequest extends Request
@@ -12,12 +13,19 @@ class ExtendedRequest extends Request
      * @param $default
      * @return array|mixed|object|null
      */
-    public function input($key = null, $default = null)
+    public function input($key = null, $default = null): mixed
     {
         $getParams = $this->get();
         $postParams = $this->post();
 
-        $data = array_merge($getParams, $postParams);
+        //头部bloc_id 与store_id 参数优先级低于自身请求体里面的参数
+        $headerParams = [];
+        if ($key === 'bloc_id' || $key === 'store_id'){
+            $headerKey = str_replace('_','-',$key);
+            $headerParams = Yii::$app->request->headers->get($headerKey);
+        }
+
+        $data = array_merge($headerParams,$getParams, $postParams);
 
         // 如果指定了 $key，则返回对应的值，否则返回整个合集
         if ($key !== null) {
