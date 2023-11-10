7531b31f13ebee4dc1b676c7c3fc49069c93bdae
wangchunsheng <34767187+yii-diandi@users.noreply.github.com>
2023-11-10 22:12:54 +0800

    修复安装bug

---
+++ frontend/backend/pro-admin/configs/config.js
@@ -0,0 +1,10 @@
+/**
+ * @Author: Wang chunsheng  email:2192138785@qq.com
+ * @Date:   2021-11-29 23:22:59
+ * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
+ * @Last Modified time: 2023-07-14 15:17:33
+ */
+
+$siteName = '智慧小店-茶室，棋牌，台球桌等无人场所',
+$apiUrl = 'https://www.ddicms.cn',
+$siteUrl = 'https://www.ddicms.cn'
