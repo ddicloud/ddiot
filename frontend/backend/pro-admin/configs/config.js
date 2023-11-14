9644b29f8e2dd290211ee9cf3ff91dfc2c1b9b37
wangchunsheng <34767187+yii-diandi@users.noreply.github.com>
2023-11-14 09:51:30 +0800

    登录页兼容手机端优化

--- frontend/backend/pro-admin/configs/config.js
+++ frontend/backend/pro-admin/configs/config.js
@@ -2,9 +2,10 @@
  * @Author: Wang chunsheng  email:2192138785@qq.com
  * @Date:   2021-11-29 23:22:59
  * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2023-07-14 15:17:33
+ * @Last Modified time: 2023-11-14 09:46:39
  */
 
-$siteName = '智慧小店-茶室，棋牌，台球桌等无人场所',
+$siteName = '智慧小店',
+$siteDesc = '智慧小店-茶室，棋牌，台球桌等无人场所',
 $apiUrl = 'https://www.ddicms.cn',
 $siteUrl = 'https://www.ddicms.cn'
